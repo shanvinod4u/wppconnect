f5adef492a87e5ee8b3aa6b0dcc59ce48aac2b91
vinoth <shanvinod4u@gmail.com>
2024-05-09 06:40:23 +0000

    add listener

--- server.js
+++ server.js
@@ -1,19 +1,65 @@
-const wppconnect = require('@wppconnect-team/wppconnect');
+const express = require('express')
+const wppConnect = require('@wppconnect-team/wppconnect')
+const request = require('request')
+const server = express()
+server.use(express.json())
+server.use(express.urlencoded({extended: true}))
 
-wppconnect
-  .create({ session: "my session" })
+const URL = "https://bingoflow.bingokart.ai/webhook/435cbf95-ea8f-4784-858c-933c2d01a6ff"
+
+
+function post_sendMessage(client){
+  server.post('/send-message', (req, res) => {
+    const w_id = req.body.w_id
+    const message = req.body.message
+  
+    client.sendText(w_id, message)
+    res.send("Message has sent successfully")
+  })
+}
+
+function post_ListenMessages(client) {
+  client.onMessage(msg => {
+    const options = {
+      "method": "POST",
+      "url": URL,
+      "headers": {
+        "Content-type": "application/json"
+      },
+      json: msg
+    }
+
+    request(options, (error) => {
+      if (error) {
+        throw new Error(error)
+      }
+    })
+  })
+}
+
+
+wppConnect
+  .create({ session: 'my_session' })
   .then((client) => { start(client) })
   .catch((error) => { console.log(error) })
 
 function start(client) {
-  console.log('client has started')
-  listen_messages(client)
+  console.log('Client has started')
+  post_ListenMessages(client)
+  // listen_messages(client)
+  post_sendMessage(client)
 }
 
 function listen_messages(client) {
-  client.onMessage((msg) => {
-    const wa_id = msg.from
-    const message = "Ask me something"
-    client.sendText(wa_id, message)
-  })
+  server.get('/messages', (req, res) => {
+    client.onMessage((msg) => {
+      console.log(msg); // Log the message when one is received
+      res.json(msg);    // Send the message back in the server response
+    });
+  });
 }
+
+
+server.listen(4000, () => {
+  console.log('Listening to port: 4000')
+})
